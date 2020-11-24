f39556883ca4320b476f719b5e7a6f9986e339f5
Niel Rohling <niel.rohling@gmail.com>
2020-11-24 15:59:07 +0100

    async func

--- index.js
+++ index.js
@@ -9,24 +9,22 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
   if (msg.author.id === '651858294478078002') {
     msg.react('🍌').then(() => 
       msg.react('🐒')
     );
   }
 
-  if (msg.mentions.has(client.user.id) || msg.content.includes('🐒')) {
+  if (msg.mentions.has(client.user.id)) {
     msg.channel.send('uh, uh, ah, ah', { tts: true });
   }
 
   if (msg.content === '🍌') {
-    fetch("https://api.tenor.com/v1/search?q=funny-monkey")
-    .then(res => res.json())
-    .then(body => {
-      const gifs = body.results.map(res => res.media[0].gif.url);
-      msg.channel.send(gifs[Math.floor(Math.random()*gifs.length)]);
-    })
+    const res = await fetch("https://api.tenor.com/v1/search?q=funny-monkey&limit=50");
+    const body = await res.json();
+    const gifs = body.results.map(res => res.media[0].gif.url);
+    msg.channel.send(gifs[Math.floor(Math.random()*gifs.length)]);   
   }
 });
 
