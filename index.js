d1398f7056782e970e3e30bfd5c17d2d492c054a
Niel Rohling <niel.rohling@gmail.com>
2020-11-24 15:53:08 +0100

    send random gif

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const fetch = require("node-fetch")
+
 const config = require("./config.json");
 
 client.on('ready', () => {
@@ -14,8 +16,17 @@ client.on('message', msg => {
     );
   }
 
-  if(msg.mentions.has(client.user.id) || msg.content.includes('🐒')) {
+  if (msg.mentions.has(client.user.id) || msg.content.includes('🐒')) {
     msg.channel.send('uh, uh, ah, ah', { tts: true });
+  }
+
+  if (msg.content === '🍌') {
+    fetch("https://api.tenor.com/v1/search?q=funny-monkey")
+    .then(res => res.json())
+    .then(body => {
+      const gifs = body.results.map(res => res.media[0].gif.url);
+      msg.channel.send(gifs[Math.floor(Math.random()*gifs.length)]);
+    })
   }
 });
 
